583d1d919a5f90a100acbd4fa35e8ce49d9803b9
Jstanion <123262263+Jstanion@users.noreply.github.com>
2023-03-06 23:28:39 -0600

    added a question array

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,13 @@
-//Create variables for timer, timeLeft, score, and currentQuestionIndex, and set them to initial values.
+//declare all global variables here
 let buttonEl = document.getElementById("buttonEl");
-let timer = document.getElementById("timer");
+let timeEl = document.getElementById("timer");
 let questionEl = document.getElementById("questionEl");
 let choiceEl = document.getElementById("choiceEl")
 
-//timer starting time
+//Create variables for timer, timeLeft, and currentQuestionIndex, and set them to initial values.
+let timer = 60;
 let timeLeft = 60;
+let currentQuestionIndex = 0;
 
 //initial page rendering
 questionEl.textContent = "Coding Quiz Challenge";
@@ -14,69 +16,103 @@ buttonEl.textContent = "START QUIZ";
 
 // Define an array of objects to store the quiz questions, where each object contains the question text, an array of answer choices, and the correct answer.
     //Create an array of objects called quizQuestions, where each object has a question property, a choices property which is an array of answer choices, and an answer property that stores the correct answer.
+    let quizQuestions = [
+        {
+            question: "Inside which HTML element do we put the JavaScript?",
+            choices: ["<javascript>", "<scripting>", "<script>", "<js>"],
+            answer: "<script>"
+        },
+        {
+            question: "Which section(s) is the correct place to insert a JavaScript link?",
+            choices: ["<body>", "<head>", "Either One", "Neither One"],
+            answer: "Either One"
+        },
+        {
+            question: "JavaScript is the same as Java",
+            choices: ["True", "False"],
+            answer: "False"
+        },
+        {
+            question: "Which event occurs when the user clicks on an HTML element?",
+            choices: ["onChange", "onMouseOver", "onMouseClick", "onClick"],
+            answer: "onClick"
+        },
+        {
+            question: "Which operator is used to assign a value to a variable?",
+            choices: ["x", "-", "*", "="],
+            answer: "="
+        }
+    ];
 
+
+// Create a function to display a question on the screen. This function should use the current question index to retrieve the question object from the array, and then display the question text and answer choices on the screen.
+    //Create a function called displayQuestion.
+let displayQuestion = function() {
+
+    
+    // Use the currentQuestionIndex to retrieve the question object from the quizQuestions array.
+    
+    
+    // Display the question text and answer choices on the screen.
+    
+    
+    // Attach an event listener to each answer choice button so that when the user clicks a button, it calls the checkAnswer function.
+    
+    
+    // Create a function to check the user's answer to a question. This function should compare the user's answer to the correct answer for the current question, and update the score and timer accordingly. It should then move on to the next question by incrementing the current question index and calling the display question function again, or end the game if all questions have been answered or the timer has reached 0.
+    //Create a function called checkAnswer that takes the user's answer as an argument.
+    
+    
+    // Use the currentQuestionIndex to retrieve the question object from the quizQuestions array.
+    
+    
+    // Compare the user's answer to the correct answer for the current question.
+    
+    
+    // If the user's answer is correct, increment the score variable and display a message indicating the answer is correct.
+    
+    
+    // If the user's answer is incorrect, decrement the timeLeft variable by 10 seconds and display a message indicating the answer is incorrect.
+    
+    
+    // Increment the currentQuestionIndex to move to the next question.
+    
+    
+    // If there are no more questions left, call the endGame function. Otherwise, call the displayQuestion function to display the next question.
+    
+};
 
 // Create a function to start the game when the user clicks the start button. This function should initialize the global variables, start the timer, and display the first question.
     //    Create a function called startGame.
-let startQuiz = function() {
+    let startQuiz = function() {
 
+        //remove introduction content
+        questionEl.textContent = "";
+        choiceEl.textContent = "";
+        buttonEl.style.display = "none";
     
-    // Set the initial values for the global variables.
+        // Set the initial values for the global variables.
+        
     
-    // Start the timer using setInterval, which will decrement the timeLeft variable by 1 every second and update the timer display on the screen.
-    let countdown = function() {
-
-        let timerInterval = setInterval(function() {
-            timeLeft--;
-            timer.textContent = "Time: " + timeLeft + " seconds left";
-            
-            if(timeLeft <= 0) {
-                timer.textContent = "Out of time!";
-                endGame();
-            };
-        }, 1000);
+        // Start the timer using setInterval, which will decrement the timeLeft variable by 1 every second and update the timer display on the screen.
+        let countdown = function() {
+    
+                setInterval(function() {
+                timeLeft--;
+                timeEl.textContent = "Time: " + timeLeft + " seconds left";
+                
+                if(timeLeft <= 0) {
+                    timeEl.textContent = "Out of time!";
+                    endGame();
+                };
+            }, 1000);
+        };
+        countdown();
+    
+        // Call the displayQuestion function to display the first question.
+        displayQuestion();
     };
-    countdown();
-
-    // Call the displayQuestion function to display the first question.
     
-    
-}
-// Create a function to display a question on the screen. This function should use the current question index to retrieve the question object from the array, and then display the question text and answer choices on the screen.
-    //Create a function called displayQuestion.
-
-
-    // Use the currentQuestionIndex to retrieve the question object from the quizQuestions array.
-
-
-    // Display the question text and answer choices on the screen.
-
-
-    // Attach an event listener to each answer choice button so that when the user clicks a button, it calls the checkAnswer function.
-
-
-// Create a function to check the user's answer to a question. This function should compare the user's answer to the correct answer for the current question, and update the score and timer accordingly. It should then move on to the next question by incrementing the current question index and calling the display question function again, or end the game if all questions have been answered or the timer has reached 0.
-    //Create a function called checkAnswer that takes the user's answer as an argument.
-
-
-    // Use the currentQuestionIndex to retrieve the question object from the quizQuestions array.
-
-
-    // Compare the user's answer to the correct answer for the current question.
-
-
-    // If the user's answer is correct, increment the score variable and display a message indicating the answer is correct.
-
-
-    // If the user's answer is incorrect, decrement the timeLeft variable by 10 seconds and display a message indicating the answer is incorrect.
-
-
-    // Increment the currentQuestionIndex to move to the next question.
-
-
-    // If there are no more questions left, call the endGame function. Otherwise, call the displayQuestion function to display the next question.
-
-
 // Create a function to end the game when all questions have been answered or the timer has reached 0. This function should stop the timer, display the final score on the screen, and provide a way for the user to enter their initials and save their score.
     //Create a function called endGame.
     function endGame() {
